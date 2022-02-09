290f038eaf24e3fdb3995c83a8e4b1d7661f6f89
OLIPLICHE <olipliche@gmail.com>
2022-02-09 12:23:58 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greeting_1 = Messages.create(greeting: "Hello Yannck, How you doing?")
+greeting_2 = Messages.create(greeting: "Hello Paka, I'am fine et toi")
+greeting_3 = Messages.create(greeting: "Je me porte bien, Bonjour kOSSI")
+greeting_4 = Messages.create(greeting: "Hella Hella, Donard")
+greeting_5 = Messages.create(greeting: "Mbote na bino")
