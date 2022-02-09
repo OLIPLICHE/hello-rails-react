d11fb6315d16db27b737b5a06cb7aacba067d854
OLIPLICHE <olipliche@gmail.com>
2022-02-09 13:23:54 +0100

    Generate react component

--- app/controllers/v1/messages_controller.rb
+++ app/controllers/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class V1::MessagesController < ApplicationController
   def index
   @messages = Messages.all.sample(1)
-  render json: { :messages => @messages }.to_json
+  render json: { messages: @messages }.to_json
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-greeting_1 = Messages.create(greeting: "Hello Yannck, How you doing?")
-greeting_2 = Messages.create(greeting: "Hello Paka, I'am fine et toi")
-greeting_3 = Messages.create(greeting: "Je me porte bien, Bonjour kOSSI")
-greeting_4 = Messages.create(greeting: "Hella Hella, Donard")
-greeting_5 = Messages.create(greeting: "Mbote na bino")
+greeting_1 = Message.create(greeting: "Hello Yannck, How you doing?")
+greeting_2 = Message.create(greeting: "Hello Paka, I'am fine et toi")
+greeting_3 = Message.create(greeting: "Je me porte bien, Bonjour kOSSI")
+greeting_4 = Message.create(greeting: "Hella Hella, Donard")
+greeting_5 = Message.create(greeting: "Mbote na bino")
